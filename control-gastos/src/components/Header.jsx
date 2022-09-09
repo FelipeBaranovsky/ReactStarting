31dfc12674671f38db72ef815e4db25b329e6cc6
FelipeBaranovsky <fbaranovsky@gmail.com>
2022-09-09 00:47:26 -0300

    Control-gastos terminado

--- control-gastos/src/components/Header.jsx
+++ control-gastos/src/components/Header.jsx
@@ -1,13 +1,21 @@
+import ControlPresupuesto from "./ControlPresupuesto"
 import NuevoPresupuesto from "./NuevoPresupuesto"
 
-const Header = ({presupuesto,setPresupuesto,isValidPresupuesto,setIsValidPresupuesto}) => {
+const Header = ({presupuesto,setPresupuesto,isValidPresupuesto,setIsValidPresupuesto,gastos,setGastos,setFiltro}) => {
   return (
     <header>
         <h1>
             Control de gastos
         </h1>
         {isValidPresupuesto ? (
-            <p>Control Presupuesto</p>
+            <ControlPresupuesto
+                gastos={gastos}
+                setGastos = {setGastos}
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+                setIsValidPresupuesto={setIsValidPresupuesto}
+                setFiltro={setFiltro}
+            />
         ) : (
             <NuevoPresupuesto
                 presupuesto={presupuesto}
