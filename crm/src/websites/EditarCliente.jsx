10d3fae2486765bf1c096806743d627c7c431814
FelipeBaranovsky <fbaranovsky@gmail.com>
2022-09-17 11:08:22 -0300

    Setting Editing

--- crm/src/websites/EditarCliente.jsx
+++ crm/src/websites/EditarCliente.jsx
@@ -6,7 +6,7 @@ const EditarCliente = () => {
 
   const [cliente, setCliente] = useState({});
   const [cargando, setCargando] = useState(true);
-  const { id } = useParams();
+  const { id } = useParams()
 
   useEffect(() => {
     const obtenerClienteAPI = async () => {
@@ -27,9 +27,12 @@ const EditarCliente = () => {
     <div>
       <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
       <p className="mt-3">Utiliza este formulario para editar datos de un cliente</p>
-      <Formulario
-        cliente={cliente}
-      />
+      {cliente?.nombre ? (
+        <Formulario
+          cliente={cliente}
+          cargando={cargando}
+        />
+      ) : <p>ID no encontrado</p>} 
     </div>
   )
 }
