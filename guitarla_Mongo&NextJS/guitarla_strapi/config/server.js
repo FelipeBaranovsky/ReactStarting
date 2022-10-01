0d514b0af8ad4aeeb3f09d408d6c61f69e9a430b
FelipeBaranovsky <fbaranovsky@gmail.com>
2022-10-01 12:35:26 -0300

    GuitarLA finished

--- guitarla_Mongo&NextJS/guitarla_strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '334678e129d2d2e3da3b69d0d7bd62bb'),
-    },
-  },
-});
--- guitarla_Mongo&NextJS/guitarla_strapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '6bd5560a-93cc-453c-a6f7-3cd3290c7528'
-};
