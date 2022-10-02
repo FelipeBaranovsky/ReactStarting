effdc86cf4d0ad30822963bd6ca788f0fe401d10
FelipeBaranovsky <fbaranovsky@gmail.com>
2022-10-02 16:37:22 -0300

    Cotizador finished

--- cotizador/src/App.jsx
+++ cotizador/src/App.jsx
@@ -1,9 +1,12 @@
 import AppSeguro from "./components/AppSeguro"
+import { CotizadorProvider } from "./context/CotizadorProvider"
 
 function App() {
 
   return (
-    <AppSeguro/>
+    <CotizadorProvider>
+      <AppSeguro/>
+    </CotizadorProvider>
   )
 }
 
--- cotizador/src/components/Formulario.jsx
+++ cotizador/src/components/Formulario.jsx
@@ -1,15 +1,33 @@
 import { Fragment } from "react"
 import { MARCAS,YEARS,PLANES } from "../constants"
+import useCotizador from "../hooks/useCotizador"
+import Error from "./Error"
 
 const Formulario = () => {
+
+    const {datos,handleChangeDatos,error,setError,cotizarSeguro} = useCotizador()
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if(Object.values(datos).includes('')){
+            setError('Todos los campos son obligatorios');
+            return
+        }
+        setError('')
+        cotizarSeguro()
+    }
+    
+    
+
   return (
     <>
-        <form>
+        {error && <Error/>}
+        <form onSubmit={handleSubmit}>
             <div className="my-5">
                 <label className="block mb-3 font-bold text-gray-400 uppercase">
                     Marca
                 </label>
-                <select name="marca" className="w-full p-3 bg-white border border-gray-200">
+                <select name="marca" className="w-full p-3 bg-white border border-gray-200" onChange={e => handleChangeDatos(e)} value={datos.marca}>
                     <option value="">-- Selecciona una Marca --</option>
                     {MARCAS.map(marca => (
                         <option key={marca.id} value={marca.id}>{marca.nombre}</option>
@@ -20,7 +38,7 @@ const Formulario = () => {
                 <label className="block mb-3 font-bold text-gray-400 uppercase">
                     Año
                 </label>
-                <select name="year" className="w-full p-3 bg-white border border-gray-200">
+                <select name="year" className="w-full p-3 bg-white border border-gray-200" onChange={e => handleChangeDatos(e)}  value={datos.year}>
                     <option value="">-- Selecciona el año --</option>
                     {YEARS.map(year => (
                         <option key={year} value={year}>{year}</option>
@@ -35,7 +53,7 @@ const Formulario = () => {
                     {PLANES.map(plan => (
                         <Fragment key={plan.id}>
                             <label>{plan.nombre}</label>
-                            <input type="radio" name="plan" value={plan.id} />
+                            <input type="radio" name="plan" value={plan.id} onChange={e => handleChangeDatos(e)}/>
                         </Fragment>
                     ))}
                 </div>
