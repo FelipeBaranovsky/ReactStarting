d7931a7d5f409b89f3ccdaeb44f1799b0fa1c1d6
FelipeBaranovsky <fbaranovsky@gmail.com>
2022-10-08 19:34:29 -0300

    Starting Quiosco App

---
+++ quioscoapp/context/QuioscoProvider.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { useState, useEffect, createContext } from "react";
+
+const QuioscoContext = createContext()
+
+const QuioscoProvider = ({children}) => {
+
+    const [categorias, setCategorias] = useState([])
+    const [categoriaActual, setCategoriaActual] = useState({})
+
+    useEffect(() => {
+        obtenerCategorias()
+    }, [])
+
+    useEffect(() => {
+        setCategoriaActual(categorias[0])
+    }, [categorias])
+
+    const obtenerCategorias = async () => {
+        try {
+            const {data} = await axios('/api/categorias')
+            setCategorias(data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleClickCategoria = id => {
+        const categoria = categorias.filter(cat => cat.id === id)
+        setCategoriaActual(categoria[0])
+    }
+    
+
+    return (
+        <QuioscoContext.Provider value={{
+            categorias,
+            categoriaActual,
+            handleClickCategoria
+            }}>
+            {children}
+        </QuioscoContext.Provider>
+    )
+}
+
+export {
+    QuioscoProvider
+}
+
+export default QuioscoContext
